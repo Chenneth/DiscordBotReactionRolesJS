8858108fd16f3dcf8a064c3ecd000832e5bc9c50
Kenneth Chen <74205780+Chenneth@users.noreply.github.com>
2022-10-29 11:54:02 -0500

    Started using fetch methods properly
    Using then() and await now because I didn't know it was async before

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, Events, GatewayIntentBits, Partials, Collection, TextChannel} = require('discord.js');
 
+const { Client, Events, GatewayIntentBits, Partials, Collection} = require('discord.js');
+
+//will someone please tell me how to typescript json properly?
 /**
- * @type {} watchableMessages
+ * @type {any} watchableMessages
  * @type {string} watchableMessages.id
  * @type {string} watchableMessages.channelId
  * @type {string} watchableMessages.guildId
@@ -17,22 +19,20 @@ const client = new Client({
 });
 
 /**
- * @type {Collection<import("discord.js").Snowflake,Guild>}
+ * @type {Collection<import("discord.js").Snowflake,Guild>} A cached collection of the guilds the bot is in
  */
 const guilds = client.guilds.cache;
 
 //Values should Look like <guildId, <emojiId, Role>>
                                 //Role is the discord object Role, the rest are a string representing the id
 /**
- *
- * @type {Map<string, Map<string,import("discord.js").Role>>}
+ * @type {Map<string, Map<string,import("discord.js").Role>>} A map that takes the id of the guild and maps it to another map whose key is the emoji's id and value is the Role object from Discord.JS
  */
 emojiToRole = new Map;
 
-//Values should look like <messageId, Collection<Snowflake,User> >... maybe?
+//Values should look like <messageId, Map<emojiId,Collection<Snowflake,User> >>... maybe?
 /**
- *
- * @type {Map<string, UserManager>}
+ * @type {Map<string, Map<string,Collection<import("discord.js").Snowflake,User>>>}
  */
 oldUsers = new Map;
 
@@ -63,10 +63,45 @@ client.on("ready", ()=>
                 if(!channel.isTextBased)
                 {
                     console.log("I found a channel that isn't text based you nerd.");
+                    return;
                 }
                 else {
-                                                                            //no idea why but WebStorm marks messages as undefined (it's not I literally ran this without error)
-                    oldUsers.set(message.id, client.channels.cache.get(channelId).messages.fetch(message.id).reactions.cache.fetch(watchableMessages.emojiIds[0])); //i'm really lazy right now and am just testing to make sure this works
+                    client.channels.cache.get(channelId).messages.fetch(message.id).then(
+                        /*Success*/(messageMatch)=>{
+                            emojis.forEach((emoji)=> {
+                                //for consistency's sake, we add each reaction we monitor to the message (so numbers aren't weirdly skewed)
+                                emoji.messageIds.forEach((emojiMessageId)=> {
+                                    if(emojiMessageId==messageMatch.id)
+                                    {
+                                        console.log(`Adding emoji with ID ${emoji.id} to message with ID ${emojiMessageId}`);
+                                        messageMatch.react(emoji.id);
+                                        if(oldUsers.has(messageMatch.id))
+                                            oldUsers.get(messageMatch.id).set(emoji.id,messageMatch.reactions.cache.get(emojiMessageId).users.cache);
+                                        else
+                                            oldUsers.set(messageMatch.id,new Map([[emoji.id,messageMatch.reactions.cache.get(emojiMessageId).users.cache]])) //i'm going to laugh if this fails
+                                    }
+                                })
+                            })
+                        },
+                        /*Failed to find message*/()=>{
+                            console.log(`Could not find message with the given ID`);
+                        }
+                    )
+                    /*let messageMatch = client.channels.cache.get(channelId).messages.fetch(message.id);
+                    if (messageMatch==null)
+                    {
+                        console.log(`Could not find message with the given ID ${message.id}`);
+                        return;
+                    }
+                    if(messageMatch.reactions.cache==null)
+                    {
+                        console.log(`No reactions for given message. Adding reaction`);
+                        messageMatch.reactions.
+                    }
+                    else {
+                                                                                //no idea why but WebStorm marks messages as undefined (it's not I literally ran this without error)
+                        oldUsers.set(message.id, messageMatch.reactions.cache.fetch(watchableMessages.emojiIds[0]).users.cache); //i'm really lazy right now and am just testing to make sure this works
+                    }*/
                 }
             }
         }
@@ -74,25 +109,28 @@ client.on("ready", ()=>
     emojis.forEach((emojiItem)=>
     {
         guild = client.guilds.cache.get(emojiItem.guildId);
-        if(guild == null || guild.roles.fetch(emojiItem.roleId)==null) //make sure the guild exists and that it has the role we need to check
+        if(guild == null /*|| guild.roles.fetch(emojiItem.roleId)==null*/) //make sure the guild exists and that it has the role we need to check
                                                             //we do not check for the emoji because nitro users exist :barf:
         {
-            console.log("couldn't find guild with specified id, or the guild didn't have the role")
+            console.log(`couldn't find guild with specified id ${emojiItem.guildId}`)
             return;
         }
 
-        //maps the emojiId to the cached role (so we don't fetch it every time we see the reaction)
-        //emojiToRole.set(item.id,guild.roles.fetch(item.roleId))
-        if(emojiToRole.has(emojiItem.guildId)) //if the guild has already been added to the map
-        {
-            //we need to get the first map to set the nested map so we can add a new value in it
-            emojiToRole.get(emojiItem.guildId).set(emojiItem.id,guild.roles.fetch(emojiItem.roleId));
-        }
-        else //we haven't added the guild to the map and we can use set
-        {
-            //anyways I was really stupid and you need double square brackets to make an entry (array of arrays because i can't just use <> syntax or something (stupid js amirite)
-            emojiToRole.set(emojiItem.guildId, new Map([[emojiItem.id,guild.roles.fetch(emojiItem.roleId)]]));
-        }
+        guild.roles.fetch(emojiItem.roleId).then((roleToAdd)=>{
+            if(emojiToRole.has(emojiItem.guildId)) //if the guild has already been added to the map
+            {
+                //we need to get the first map to set the nested map so we can add a new value in it
+                emojiToRole.get(emojiItem.guildId).set(emojiItem.id,roleToAdd);
+            }
+            else //we haven't added the guild to the map and we can use set
+            {
+                //anyways I was really stupid and you need double square brackets to make an entry (array of arrays because i can't just use <> syntax or something (stupid js amirite)
+                emojiToRole.set(emojiItem.guildId, new Map([[emojiItem.id,roleToAdd]]));
+            }
+        },()=>{
+            console.log(`Couldn't find a role in the guild with the id ${emojiItem.roleId}`)
+        })
+
     })
 });
 
@@ -102,7 +140,7 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
         // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
         try {
             await reaction.fetch();
-        } catch (error) { //i love that javascript doesn't have a way to catch only specific errors
+        } catch (error) {
             console.error('Something went wrong when fetching the message:', error);
             // Return as `reaction.message.author` may be undefined/null
             return;
@@ -121,12 +159,14 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
     (await reaction.users.fetch()).forEach((user,snowflake,map)=>
     {
         let newUser = oldUsers[reaction.message.id].fetch(user);
-        if(newUser==null)
+        if(newUser==null) //will be null if we couldn't fetch the new user from the oldUsers list
         {
-            return;
+            console.log(`Found a new user `)
+            reaction.message.guild.members.fetch(newUser).roles.add(emojiToRole.get(reaction.message.guildId).get(reaction.emoji.id));
         }
-        reaction.message.guild.members.fetch(newUser).roles.add(emojiToRole.get(reaction.message.guildId).get(reaction.emoji.id));
-    })
+
+    });
+    oldUsers.set(reaction.message.id,reaction.users.cache);
 /*
     // Now the message has been cached and is fully available
     console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
@@ -178,9 +218,9 @@ client.login(token);
 
 /**
  *
- * @param {import("discord.js").Emoji} emoji
- * @param {string|import("discord.js").Snowflake} emoji.id
- * @returns {boolean}
+ * @param {import("discord.js").Emoji} emoji The Emoji object of the emoji you want to confirm its existence in the array
+ * @param {string|import("discord.js").Snowflake} emoji.id The id of the emoji, either a string or 'Snowflake' as Discord calls it (both are still comparable)
+ * @returns {boolean} Whether the emoji is in the emojis JSON array
  */
 function hasEmoji(emoji)
 {
@@ -194,10 +234,9 @@ function hasEmoji(emoji)
 }
 
 /**
- *
- * @param {import("discord.js").Snowflake|string} value
- * @param {string} valueType
- * @returns {boolean}
+ * @param {import("discord.js").Snowflake|string} value The value we want to find in the watchableMessages array
+ * @param {string} valueType The name of the property from watchableMessages we are comparing to
+ * @returns {boolean} Whether the watchableMessages array has an element with a property with the same value as value
  */
 function watchableMessagesHasVal(value, valueType)
 {
